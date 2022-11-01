c42c15c5ec0809d6f6acdee59725f0dadd6f61be
ahmedabzk <ahmed.abzk@gmail.com>
2022-11-01 13:26:29 +0300

    go microservices with CI/CD

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	// This creates a router that can be used to define the build of the application.
+	router := gin.Default()
+
+	// load the templates
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			http.StatusOK,
+			// use the index.html page
+			"index.html",
+			gin.H{
+				"title":"Home Page",
+			},
+		)
+	})
+	router.Run()
+}
